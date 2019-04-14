60159ce2857b6f8b970da2543572a243d5e3a5d3
Your Name <freelspringisoul@gmail.com>
2019-04-14 21:25:47 +0300

    Updates

--- src/redux/api/apiTVMaze.js
+++
@@ -1,5 +0,0 @@
-import axios from 'axios';
-
-export function getSerialsList(param){
-    return (axios.get(`http://api.tvmaze.com/search/shows?q=${param}`));
-};
--- src/redux/store/store.js
+++
@@ -1,8 +0,0 @@
-import { createStore, applyMiddleware, compose } from 'redux';
-import rootReducers from '../reducers/index.js';
-import thunk from 'redux-thunk';
-
-const DevTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
-const store = createStore(rootReducers, compose(applyMiddleware(thunk), DevTools));
-
-export default store;
